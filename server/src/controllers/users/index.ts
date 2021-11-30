6fb14de596e7eb0bd64c7f6a679e7255bc4f9a76
Syncxv <aruldevarackal1@gmail.com>
2021-11-30 06:35:34 +0530

    user is kinda done ig maybe

--- server/src/controllers/users/index.ts
+++ server/src/controllers/users/index.ts
@@ -5,13 +5,28 @@ const users = {
         const data = await User.find()
         res.send({ data })
     },
+    getUser: async (req: Request<{ id: string }>, res: Response) => {
+        try {
+            const { id } = req.query
+            const user = await User.find({ id })
+            if (!user) {
+                return res.status(404).send({ error: 'cant find him :|' })
+            }
+            console.log(user)
+            return res.send({ data: user } || { data: null })
+        } catch (err) {
+            return res.send({ error: err.message })
+        }
+    },
     register: async (req: Request<any, any, UserType>, res: Response) => {
-        const { username, password, email } = req.body
-        if (!username || !password || !email)
-            return res.send({ error: 'missing feilds' })
-        const user = await User.create({ username, password, email })
-        user.save()
-        return res.send(user)
+        try {
+            const { username, password, email } = req.body
+            const user = await User.create({ username, password, email })
+            user.save()
+            res.status(201).send(user)
+        } catch (err) {
+            res.send({ error: err.message })
+        }
     }
 }
 
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -3,6 +3,7 @@ import users from '../controllers/users'
 
 const router = Router()
 router.route('/').get(users.index)
+router.route('/getuser/:id').get(users.getUser)
 router.route('/register').post(users.register)
 
 export default { path: 'users', router }
