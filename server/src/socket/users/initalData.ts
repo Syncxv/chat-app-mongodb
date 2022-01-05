b17828c6c7bf999ad8efd6baea9e9b87ece93c3d
Syncxv <aruldevarackal1@gmail.com>
2022-01-06 00:28:06 +0530

    fixed liniting error

--- server/src/socket/users/initalData.ts
+++ server/src/socket/users/initalData.ts
@@ -1,7 +1,7 @@
 import { Socket } from 'socket.io'
 import User, { FriendType } from '../../models/user'
 export const USER_INITAL_DATA_EVENT_NAME = 'user-inital-data'
-const onUserInitalData = async (socket: Socket, cb) => {
+const onUserInitalData = async (socket: Socket, cb: any) => {
     const currentUser = await User.findById(socket.data.jwt.user.id).populate({
         path: 'friends',
         model: 'Friend'
