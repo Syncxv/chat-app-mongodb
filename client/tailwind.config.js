4a3c4f468bb6e4dd1dc14d1a0b9daf7876946d1f
Syncxv <aruldevarackal1@gmail.com>
2022-01-08 13:28:29 +0530

    added colors to tailwind :O

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,9 +8,21 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-            btnPink: '#fc0a7e',
+            btnPink: '"#fc0a7e"',
             colors: {
-                logoBg: '#5041FF'
+                'text-normal': 'var(--text-normal)',
+                primary: '#18dcff',
+                'background-primary:': '#23272a',
+                'background-secondary:': '#1a1e21',
+                'background-secondary-alt:': '#15181b',
+                'background-tertiary:': '#111416',
+                'channeltextarea-background:': '#1a1e21',
+                'background-floating:': '#111416',
+                'scrollbar-auto-thumb:': '#0000004a',
+                'scrollbar-auto-track:': '#00000036',
+                'text-input-border-hover:': '#040405',
+                'text-input-border-disabled:': '#202225',
+                'text-input-prefix:': '#dcddde'
             }
         }
     },
