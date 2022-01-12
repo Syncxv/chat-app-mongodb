7ef85fb9ff007cc28a4535f4ab6bd495d5a5b6ff
Syncxv <aruldevarackal1@gmail.com>
2022-01-12 23:06:44 +0530

    YOOOOOOOOOOOOOOO I FUCKING DID IT NO WAY :D

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -33,15 +33,26 @@ const mapDispatchToProps = (dispatch: AppDispatch) =>
         },
         dispatch
     )
-class MainClass extends React.Component<Props> {
+class MainClass extends React.Component<Props, { initalized: boolean }> {
     ref: React.RefObject<HTMLDivElement>
+    placeholderRef: React.RefObject<HTMLDivElement>
     messageListRef: React.RefObject<HTMLUListElement>
+    inited: React.RefObject<boolean>
     resizeObserver: ResizeObserver
+    intersectionObserver: IntersectionObserver
     constructor(props: Props) {
         super(props)
+        this.state = {
+            initalized: false
+        }
         this.ref = React.createRef()
         this.messageListRef = React.createRef()
+        this.placeholderRef = React.createRef()
+        this.inited = React.createRef()
         this.resizeObserver = new ResizeObserver(this.handleResize.bind(this))
+        this.intersectionObserver = new IntersectionObserver(this.handleIntersection.bind(this), {
+            threshold: 0
+        })
         this.getScrollState = this.getScrollState.bind(this)
         this.handleScroll = this.handleScroll.bind(this)
         this.props.fetchMessages({ channel_id: this.props.params?.cid })
@@ -49,6 +60,9 @@ class MainClass extends React.Component<Props> {
     componentDidMount() {
         console.log('MOUNTED', this.ref)
         this.resizeObserver.observe(this.messageListRef.current!)
+        if (this.placeholderRef.current) {
+            this.intersectionObserver.observe(this.placeholderRef.current)
+        }
     }
     componentWillUnmount() {
         this.resizeObserver.disconnect()
@@ -63,13 +77,15 @@ class MainClass extends React.Component<Props> {
             <AppWrapper>
                 <div ref={this.ref} onScroll={this.handleScroll} className="main-seciton">
                     <ul ref={this.messageListRef} className="messages-list">
-                        {hasMore && (
-                            <div className="placeholder-wrapper-iguess-idk-man">
-                                {Array.from(Array(10)).map(() => (
-                                    <MessagePlaceholder />
-                                ))}
-                            </div>
-                        )}
+                        <div ref={this.placeholderRef} className="placeholder-wrapper">
+                            {hasMore && (
+                                <div className="placeholder-wrapper-iguess-idk-man">
+                                    {Array.from(Array(10)).map(() => (
+                                        <MessagePlaceholder />
+                                    ))}
+                                </div>
+                            )}
+                        </div>
                         {finishedFetching
                             ? messages.map(message => <Message key={message._id} message={message} />)
                             : 'aw'}
@@ -80,24 +96,68 @@ class MainClass extends React.Component<Props> {
     }
 
     handleScroll(e: React.UIEvent<HTMLDivElement, UIEvent>) {
-        console.log(e)
         console.log(this.getScrollState())
     }
+
+    handleIntersection([entry]: IntersectionObserverEntry[]) {
+        setTimeout(() => {
+            console.log(this.state)
+            const scrollState = this.getScrollState()
+            const heightTakeAwayOffesetHeight = scrollState.scrollHeight - scrollState.offsetHeight
+            console.log(
+                'heightTakeAwayOffesetHeight:',
+                heightTakeAwayOffesetHeight,
+                'scrollTop',
+                scrollState.scrollTop
+            )
+
+            if (
+                !this.props.fetching.loading &&
+                this.state.initalized &&
+                entry.isIntersecting &&
+                !(heightTakeAwayOffesetHeight === scrollState.scrollTop)
+            ) {
+                //CAN FETCH MESSAGES :D HOLY SHIT
+                console.log(entry, this.getScrollState())
+                this.props.fetchMessages({
+                    channel_id: this.props.params.cid,
+                    before: this.props.messages[0]._id
+                })
+            }
+        }, 100)
+    }
+
     handleResize(e: ResizeObserverEntry[]) {
         console.log(e, e[0].target.childElementCount)
-        this.ref.current?.scrollTo(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+        const scrollState = this.getScrollState()
+        if (scrollState?.scrollTop === 0 && this.props.hasMore) {
+            this.ref.current?.scrollTo(0, Number.MAX_SAFE_INTEGER)
+            this.setState({ initalized: true })
+        }
     }
-    getScrollState() {
-        const div = this.ref.current
-        if (div) {
+    getScrollState(elem?: HTMLDivElement) {
+        if (!elem) {
+            const div = this.ref.current!
             return {
                 scrollTop: div.scrollTop,
                 scrollLeft: div.scrollLeft,
                 scrollHeight: div.scrollHeight,
                 scrollWidth: div.scrollWidth,
                 offsetHeight: div.offsetHeight,
-                offsetWidth: div.offsetWidth
+                offsetWidth: div.offsetWidth,
+                clientHeight: div.clientHeight,
+                offsetTop: div.offsetTop
             }
+        }
+        return {
+            scrollTop: elem.scrollTop,
+            scrollLeft: elem.scrollLeft,
+            scrollHeight: elem.scrollHeight,
+            scrollWidth: elem.scrollWidth,
+            offsetHeight: elem.offsetHeight,
+            offsetWidth: elem.offsetWidth,
+            clientHeight: elem.clientHeight,
+            offsetTop: elem.offsetTop
         }
     }
 }
