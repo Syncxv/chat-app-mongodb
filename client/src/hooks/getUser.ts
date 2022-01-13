dbfcc0426f6aebdc1a02d42b3bf9b1518b1c3eb4
Syncxv <aruldevarackal1@gmail.com>
2022-01-13 20:04:31 +0530

    please

--- client/src/hooks/getUser.ts
+++ client/src/hooks/getUser.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { apiUrl } from '../constants'
-import { requestWrapper } from '../util/reqeust'
 
 export const getUser = async (id: string) => {
     return (await axios.get(`${apiUrl}/users/${id}`)).data.data
