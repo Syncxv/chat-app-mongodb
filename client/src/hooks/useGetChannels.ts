830c370a1d8dd4ac7e98e594cd81109dcd3423f6
Syncxv <aruldevarackal1@gmail.com>
2022-01-13 19:09:57 +0530

    for vercel

--- client/src/hooks/useGetChannels.ts
+++
@@ -1,8 +0,0 @@
-import channelStore from '../stores/channel'
-import { RawChannel } from '../types'
-import sleep from '../util/sleep'
-
-const getChannels = (): RawChannel[] => {
-    return Object.values(channelStore.getChannels())
-}
-export default getChannels
