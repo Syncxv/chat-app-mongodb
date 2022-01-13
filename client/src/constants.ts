f29d83bde8805ceb0a1032400388445f6e8c955b
Syncxv <aruldevarackal1@gmail.com>
2022-01-13 19:55:34 +0530

    api url gang

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -7,4 +7,4 @@ export enum SOCKET_ACTIONS {
 
 export const defaultPfp =
     'https://media.discordapp.net/attachments/770534108832858132/920445840600989747/default_pfp.png?width=50&height=38'
-export const apiUrl = 'http://localhost:8000'
+export const apiUrl = process.env.APIRUL || 'http://localhost:8000'
