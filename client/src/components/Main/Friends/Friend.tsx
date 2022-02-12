ec30a9cd27250ee0429ae7fe90d208ce109da975
Syncxv <aruldevarackal1@gmail.com>
2022-02-12 13:01:33 +0530

    chat button does something now :D

--- client/src/components/Main/Friends/Friend.tsx
+++ client/src/components/Main/Friends/Friend.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { defaultPfp, FreindTypes } from '../../../constants'
+import { addChannel } from '../../../reducers/channel'
 import { acceptFriend, removeFriend } from '../../../reducers/user'
+import { AppState } from '../../../stores/store'
 import { UserType } from '../../../types'
 
 interface Props {
@@ -15,11 +18,22 @@ interface ActionButtonProps {
 }
 const ActionButton: NextPage<ActionButtonProps> = ({ user, type }) => {
     const dispatch = useDispatch()
+    const { channelStore } = useSelector((state: AppState) => state)
+    const router = useRouter()
+    const handleChatClick = () => {
+        const channel_id = Object.values(channelStore.channels).find(s =>
+            s.members.map(another => another._id).includes(user._id)
+        )?._id
+        console.log(channel_id)
+        channel_id ? router.push(`/app/channels/${channel_id}`) : dispatch(addChannel(user._id))
+    }
     switch (type) {
         case FreindTypes.FRIEND:
             return (
                 <>
-                    <button className="message-him idk man">Chat</button>
+                    <button className="message-him idk man" onClick={handleChatClick}>
+                        Chat
+                    </button>
                     <button
                         className="message-him idk man"
                         onClick={() => dispatch(removeFriend({ id: user._id }))}
